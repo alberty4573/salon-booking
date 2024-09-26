97ccde9927ade6b4258e470b259fc415b9b53e91
alberty4573 <shenlieku@gmail.com>
2024-09-26 17:24:46 +1000

    initial commit

---
+++ hair-helper/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function WelcomeScreen() {
+    return (
+        <div>
+             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                 haha its working!
+             </main>
+        </div>
+    )
+}
